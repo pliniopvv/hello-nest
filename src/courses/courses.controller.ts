dfeb7e045a032381d489939c4e220f83e5249f7d
pliniopvv <gangss2@hotmail.com>
2022-05-17 10:26:58 -0300

    +courses

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,15 +1,31 @@
 /* eslint-disable prettier/prettier */
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Res } from '@nestjs/common';
 
 @Controller('courses')
 export class CoursesController {
     @Get()
-    list() {
-        return `Cursos works!`;
+    list(@Res() res) {
+        return res.status(200).send(`Cursos works!`);
     }
 
     @Get(':id')
     findOne(@Param('id') id: number) {
         return `Curso #${id}`;
+    }
+
+    @Post()
+    @HttpCode(HttpStatus.NO_CONTENT)
+    create(@Body() body) {
+        return body;
+    }
+
+    @Patch(':id')
+    update(@Param('id') id: number, @Body() body) {
+        return `${id}\n${body}`;
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: number) {
+        return `Curso #${id} deletado`;
     }
 }
